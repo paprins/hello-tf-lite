cfa35142327483ed211da0bd1b5b74c34bc785c8
Pascal Prins <pascal.prins@foobar-it.com>
2020-03-10 09:02:11 +0100

    [wip]

--- coral/coral.py
+++ coral/coral.py
@@ -64,22 +64,22 @@ def classify(ctx):
     with picamera.PiCamera(resolution=(640, 480), framerate=30) as camera:
         camera.start_preview()
         try:
-        stream = io.BytesIO()
-        for _ in camera.capture_continuous(
-                stream, format='jpeg', use_video_port=True):
-            stream.seek(0)
-            image = Image.open(stream).convert('RGB').resize((width, height),
-                                                            Image.ANTIALIAS)
-            start_time = time.time()
-            results = classify_image(interpreter, image)
-            elapsed_ms = (time.time() - start_time) * 1000
-            label_id, prob = results[0]
-            stream.seek(0)
-            stream.truncate()
-            camera.annotate_text = '%s %.2f\n%.1fms' % (labels[label_id], prob,
-                                                        elapsed_ms)
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(
+                    stream, format='jpeg', use_video_port=True):
+                stream.seek(0)
+                image = Image.open(stream).convert('RGB').resize((width, height),
+                                                                Image.ANTIALIAS)
+                start_time = time.time()
+                results = classify_image(interpreter, image)
+                elapsed_ms = (time.time() - start_time) * 1000
+                label_id, prob = results[0]
+                stream.seek(0)
+                stream.truncate()
+                camera.annotate_text = '%s %.2f\n%.1fms' % (labels[label_id], prob,
+                                                            elapsed_ms)
         finally:
-        camera.stop_preview()
+            camera.stop_preview()
 
 if __name__ == '__main__':
   main()
